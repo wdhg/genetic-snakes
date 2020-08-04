24adceba15bcc3b026416eaff22ce092568eba43
wdhg <wdhgrant@gmail.com>
2020-08-04 14:02:50 +0100

    add instance of Show for SnakeGame

--- src/Snake.hs
+++ src/Snake.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 module Snake where
 
 type Vector
@@ -12,3 +14,17 @@ data SnakeGame
     , bounds :: Vector
     , food   :: Vector
     }
+
+
+instance Show SnakeGame where
+  show game@(SnakeGame snake (width, height) food)
+    = concatMap showRow [0..height]
+      where
+        showRow :: Int -> String
+        showRow y
+          = concatMap (showCell . (,y)) [0..width] ++ "\n"
+        showCell :: Vector -> String
+        showCell cell
+          | cell `elem` snake = "# "
+          | cell == food      = "@ "
+          | otherwise         = ". "
