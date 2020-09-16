942f9bf9179411b11013994e3b2e70eb717d175b
wdhg <wdhgrant@gmail.com>
2020-09-16 10:40:07 +0100

    implement move and test it

--- src/Snake.hs
+++ src/Snake.hs
@@ -4,20 +4,20 @@ type Vector
   = (Int, Int)
 
 up, down, left, right :: Vector
-up    = (0, -1)
-down  = (0, 1)
-left  = (-1, 0)
-right = (1, 0)
+up    = (0,-1)
+down  = (0,1)
+left  = (-1,0)
+right = (1,0)
 
 data Snake
   = Snake
     { body      :: [Vector]
     , direction :: Vector
     }
-    deriving (Show)
+    deriving (Show, Eq)
 
-data SnakeGame
-  = SnakeGame
+data Game
+  = Game
     { snake  :: Snake
     , bounds :: Vector
     , food   :: Vector
@@ -27,3 +27,19 @@ data SnakeGame
 add :: Vector -> Vector -> Vector
 add (x0, y0) (x1, y1)
   = (x0 + x1, y0 + y1)
+
+move :: Snake -> Snake
+move snake'
+  = let newHead = add (head $ body snake') $ direction snake'
+     in snake' {body = newHead : (init $ body snake')}
+
+setDirection :: Vector -> Snake -> Snake
+setDirection dir snake'
+  | (add dir $ direction snake') == (0,0)
+    = snake' -- can't move back on self
+  | otherwise
+    = snake' {direction = dir}
+
+update :: Vector -> Game -> Game
+update dir game
+  = game {snake = move $ setDirection dir $ snake game}
--- test/SnakeSpec.hs
+++ test/SnakeSpec.hs
@@ -1,7 +1,23 @@
-module SnakeTests where
+module SnakeSpec where
 
+import Snake
 import Test.Hspec
+
+testGame :: Game
+testGame
+  = Game
+    { snake  = Snake [(2,0), (1,0), (0,0), (0,1), (0,2)] right
+    , bounds = (4,4)
+    , food   = (3,1)
+    }
+
+runUpdates :: [Vector] -> Game
+runUpdates
+  = foldl (flip update) testGame -- foldl so moves work in order provided
 
 spec :: Spec
 spec
-  = undefined
+  = describe "update" $ do
+      it "should move the snake properly" $ do
+        let result = snake $ runUpdates [right, down, down, left]
+        result `shouldBe` (Snake [(2,2), (3,2), (3,1), (3,0), (2,0)] left)
