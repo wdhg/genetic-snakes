1e8e53f27edf3a934d1fa1948a78eef0a2439acd
wdhg <wdhgrant@gmail.com>
2020-09-18 12:35:25 +0100

    implement mutateLink and refactor

--- src/Neat.hs
+++ src/Neat.hs
@@ -1,75 +1,137 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Neat where
 
 import Control.Monad.Random
 import Control.Monad.State
 
-data NodeType
-  = Input | Output | Hidden
-    deriving (Show, Eq)
+newtype InnovationID
+  = InnovationID Int deriving (Show, Eq, Ord, Enum)
 
-data Node
-  = Node
-    { nodeType :: NodeType
-    , uniqueID :: Int
-    }
-    deriving (Show, Eq)
+newtype NodeID
+  = NodeID Int deriving (Show, Eq, Ord, Enum)
+
+data Link
+  = Link
+    { inNode  :: NodeID
+    , outNode :: NodeID
+    } deriving (Show, Eq)
 
 data Gene
   = Gene
-    { inNode  :: Node
-    , outNode :: Node
-    , weight  :: Float
-    , enabled :: Bool
-    , innovID :: Int
+    { link         :: Link
+    , weight       :: Float
+    , enabled      :: Bool
+    , innovationID :: InnovationID
     }
     deriving (Show)
 
 instance Eq Gene where
   gene0 == gene1
-    = innovID gene0 == innovID gene1
+    = innovationID gene0 == innovationID gene1
 
-type Genome
-  = [Gene]
+data Genome
+  = Genome
+    { genes   :: [Gene]
+    , inputs  :: [NodeID]
+    , outputs :: [NodeID]
+    , hidden  :: [NodeID]
+    }
+    deriving (Show, Eq)
 
 type Innovations
-  = [(Node, Node)]
+  = [(Link, InnovationID)]
 
-perturbWeight :: RandomGen g => Gene -> Rand g Gene
+perturbWeight :: MonadRandom m => Gene -> m Gene
 perturbWeight gene
   = do
     adjustment <- getRandomR (-1.0, 1.0)
     return $ gene {weight = weight gene + adjustment}
 
-perturbWeights :: RandomGen g => Genome -> Rand g Genome
-perturbWeights
-  = mapM perturbWeight
+perturbWeights :: MonadRandom m => Genome -> m Genome
+perturbWeights genome
+  = do
+    genes' <- mapM perturbWeight $ genes genome
+    return $ genome {genes = genes'}
 
-newHiddenNode :: Genome -> Node
-newHiddenNode genome
-  = Node Hidden $ 1 + (maximum $ map uniqueID nodes)
-    where
-      nodes = map inNode genome ++ map outNode genome
+addHiddenNode :: Genome -> (NodeID, Genome)
+addHiddenNode genome
+  = case hidden genome of
+      []    -> let nodeCount = (length $ inputs genome) + (length $ outputs genome)
+                in (NodeID nodeCount, genome {hidden = [NodeID nodeCount]})
+      nodes -> (succ $ maximum nodes, genome)
 
-getInnovationID :: Monad m => (Node, Node) -> StateT Innovations m Int
+getInnovationID :: MonadRandom m => Link -> StateT Innovations m InnovationID
 getInnovationID link
   = do
     innovations <- get
-    put (innovations ++ [link])
-    return $ length innovations
+    case lookup link innovations of
+      Just innovID -> return innovID
+      Nothing -> do
+        let innovID = InnovationID $ length innovations
+        put $ innovations ++ [(link, innovID)]
+        return innovID
 
-createInOutGenes :: Monad m => Gene -> Node -> StateT Innovations m (Gene, Gene)
-createInOutGenes gene newNode
+addLink :: MonadRandom m => Link -> Float -> Genome -> StateT Innovations m Genome
+addLink link weight genome
   = do
-    inInnovID <- getInnovationID (inNode gene, newNode)
-    outInnovID <- getInnovationID (newNode, outNode gene)
-    return ( Gene (inNode gene) newNode 1.0 True inInnovID
-           , Gene newNode (outNode gene) (weight gene) True outInnovID
-           )
+    innovID <- getInnovationID link
+    let gene = Gene link weight True innovID
+     in return $ genome {genes = gene : genes genome}
 
-mutateNode :: RandomGen g => Genome -> StateT Innovations (Rand g) Genome
+disable :: Gene -> Gene
+disable gene
+  = gene {enabled = False}
+
+selectAndDisableRandomGene :: MonadRandom m => Genome -> m (Gene, Genome)
+selectAndDisableRandomGene genome
+  = do
+    index <- getRandomR (0, (length $ genes genome) - 1)
+    let (before, gene : after) = splitAt index $ genes genome
+        genome' = genome {genes = before ++ [disable gene] ++ after}
+    return (gene, genome')
+
+mutateNode :: MonadRandom m => Genome -> StateT Innovations m Genome
 mutateNode genome
   = do
-    index <- getRandomR (0, length genome - 1)
-    let (before, gene : after) = splitAt index genome
-    (geneIn, geneOut) <- createInOutGenes gene $ newHiddenNode genome
-    return $ geneOut : geneIn : before ++ [gene {enabled = False}] ++ after
+    (gene, genome') <- selectAndDisableRandomGene genome
+    let (Link inNode outNode) = link gene
+        (hiddenNode, genome'') = addHiddenNode genome'
+        addInGene = addLink (Link inNode hiddenNode) 1.0
+        addOutGene = addLink (Link hiddenNode outNode) (weight gene)
+    (addInGene >=> addOutGene) genome''
+
+getIncommingNodes :: Genome -> NodeID -> [NodeID]
+getIncommingNodes genome node
+  = let immediate = map inNode $ filter ((== node) . outNode ) $ map link $ genes genome
+     in immediate ++ concatMap (getIncommingNodes genome) immediate
+
+isNotCyclic :: Genome -> Link -> Bool
+isNotCyclic genome (Link nodeIn nodeOut)
+  = nodeIn /= nodeOut && nodeOut `notElem` (getIncommingNodes genome nodeIn)
+
+isValidLink :: Genome -> Link -> Bool
+isValidLink genome link'
+  = let existingLinks = map link $ genes genome
+     in isNotCyclic genome link' && link' `notElem` existingLinks
+
+getUnlinked :: Genome -> [Link]
+getUnlinked genome
+  = let allLinks = zipWith Link (inputs genome ++ hidden genome) (outputs genome ++ hidden genome)
+     in filter (isValidLink genome) allLinks
+
+addRandomLink :: MonadRandom m => Link -> Genome -> StateT Innovations m Genome
+addRandomLink link genome
+  = do
+    weight <- getRandomR (-2.0, 2.0)
+    addLink link weight genome
+
+mutateLink :: MonadRandom m => Genome -> StateT Innovations m Genome
+mutateLink genome
+  = do
+    maybeLink <- uniformMay $ getUnlinked genome
+    case maybeLink of
+      Nothing -> return genome -- genome is fully connected already
+      Just link -> do
+        weight <- getRandomR (-2.0, 2.0)
+        addLink link weight genome
--- test/NeatSpec.hs
+++ test/NeatSpec.hs
@@ -6,15 +6,17 @@ import Neat
 import Test.Hspec
 
 g0, g1, g2 :: Gene
-g0 = Gene (Node Input 0) (Node Output 1) 4.0 True 0
-g1 = Gene (Node Input 0) (Node Hidden 2) 1.0 True 1
-g2 = Gene (Node Hidden 2) (Node Output 1) 4.0 True 2
+g0 = Gene (Link (NodeID 0) (NodeID 1)) 4.0 True (InnovationID 0)
+g1 = Gene (Link (NodeID 0) (NodeID 2)) 1.0 True (InnovationID 1)
+g2 = Gene (Link (NodeID 2) (NodeID 1)) 4.0 True (InnovationID 2)
 
 genome0, genome1 :: Genome
 genome0
-  = [g2, g1, g0 {enabled = False}]
+  = Genome [g2, g1, g0 {enabled = False}] [NodeID 0] [NodeID 1] [NodeID 2]
 genome1
-  = [g0]
+  = Genome [g0] [NodeID 0] [NodeID 1] []
+genome2
+  = Genome [] [NodeID 0] [] [NodeID 1]
 
 spec :: Spec
 spec
@@ -22,9 +24,16 @@ spec
     describe "perturbWeights" $ do
       it "should adjust weights of genes in genome" $ do
         let result = evalRand (perturbWeights genome0) (mkStdGen 0)
-        map weight result `shouldNotBe` map weight genome0
+        map weight (genes result) `shouldNotBe` map weight (genes genome0)
 
     describe "mutateNode" $ do
       it "should replace a gene with two new genes" $ do
-        let result = evalRand (evalStateT (mutateNode genome1) [(Node Input 0, Node Output 1)]) (mkStdGen 0)
+        let result = evalRand (evalStateT (mutateNode genome1) [(Link (NodeID 0) (NodeID 1), InnovationID 0)]) (mkStdGen 0)
         result `shouldBe` genome0
+
+    describe "mutateLink" $ do
+      it "should create a new link random between unlinked nodes" $ do
+        let result = evalRand (evalStateT (mutateLink genome2) [(Link (NodeID 2) (NodeID 1), InnovationID 0) ]) (mkStdGen 0)
+        (length $ genes result) `shouldBe` 1
+        (innovationID $ head $ genes result) `shouldBe` (InnovationID 1)
+        (link $ head $ genes result) `shouldBe` (Link (NodeID 0) (NodeID 1))
